a225c91aec98fd60d4b6c674b792309ebdd486c4
merciof <mercio.andrade@ufrpe.br>
2019-06-25 16:45:11 -0300

    Criação da classe Book correspondente a collection books.

---
+++ polyglot-persistence/grails-app/domain/polyglot/persistence/Book.groovy
@@ -0,0 +1,22 @@
+package polyglot.persistence
+
+import grails.mongodb.MongoEntity
+
+class Book implements MongoEntity<Book> {
+    
+    String title
+    String isbn
+    Integer pageCount
+    Date publishDate
+    String thumbnailUrl
+    String shortDescription
+    String largeDescription
+    String status
+    String[] authors
+    String [] categories
+    
+    static mapWith = "mongo"
+
+    static constraints = {
+    }
+}
