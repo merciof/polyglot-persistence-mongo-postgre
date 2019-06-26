991be53919997ded06b2905f98c26d63678d0ec4
merciof <mercio.andrade@ufrpe.br>
2019-06-26 02:09:47 -0300

    Criação de classe User que se comunica com o postgreSQL.

---
+++ polyglot-persistence/grails-app/domain/polyglot/persistence/User.groovy
@@ -0,0 +1,19 @@
+package polyglot.persistence
+
+class User {
+    
+    String first_name
+    String last_name
+    String username
+    String password
+    String email
+    String saldo
+    Date dataNascimento
+    
+    static mapping = {
+        table 'usuarios'
+    }
+
+    static constraints = {
+    }
+}
